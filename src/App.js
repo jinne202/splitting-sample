695989e1d5bb2483b2b46854ea053b0a48da06db
jinne202 <hanb5468@gmail.com>
2020-01-30 17:16:59 +0900

    use suspense, React.lazy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SplitMe = React.lazy(() => import('./SplitMe'));
+
+function App(){
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+        setVisible(true);
+  };
+  return(
+      <div className = "App">
+          <header className = "App-header">
+              <img src={logo} className = "App-logo" alt="logo"/>
+              <p onClick = {onClick}>Hello React!</p>
+              <Suspense fallback={<div>loading...</div>}>
+                  {visible && <SplitMe/>}
+              </Suspense>
+          </header>
+      </div>
+  )
 }
 
 export default App;
